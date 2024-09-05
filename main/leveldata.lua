242cf1239bd8488737b96653867b010572ae4b3d
Evan Prock <evan.prock@gmail.com>
2024-09-04 22:50:14 -0700

    difficulty selection polish and text framework

--- main/leveldata.lua
+++ main/leveldata.lua
@@ -2,6 +2,10 @@ local D = {}
 
 D.highscore = {}
 
+D.header = {"-TINY-", "-EASY-", "-NORMAL-", "-HARD-", "-INSANE-", "-CUSTOM-"}
+
+D.description = {"[fill this in]", "OG minefinder size", "[fill this in]", "grab the glasses", "patience is KEY", "gay mode???"}
+
 D.diff = 3
 D.diff_temp = nil
 
