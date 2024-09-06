ea50bdb7d67eb0171b21bb7653206c227bab548a
Evan Prock <evan.prock@gmail.com>
2024-09-06 11:02:33 -0700

    mac/pc sync

---
+++ main/gui-nodes.lua
@@ -0,0 +1,5 @@
+local N = {}
+
+N.get = {}
+
+return N
--- main/leveldata.lua
+++ main/leveldata.lua
@@ -4,7 +4,10 @@ D.highscore = {}
 
 D.header = {"-TINY-", "-EASY-", "-NORMAL-", "-HARD-", "-INSANE-", "-CUSTOM-"}
 
-D.description = {"[fill this in]", "OG minefinder size", "[fill this in]", "grab the glasses", "patience is KEY", "gay mode???"}
+D.description = {"[fill this in]", "OG minefinder size", "[fill this in]", "grab the glasses", "patience is KEY", "~[max: 32x32]"}
+
+D.mine_nums = {10, 14, 40, 100, 150, 40}
+D.size_nums = {{8, 10, 16, 16, 24, 16}, {8, 10, 16, 30, 30, 16}}
 
 D.diff = 3
 D.diff_temp = nil
