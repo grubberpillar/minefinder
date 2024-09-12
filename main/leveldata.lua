1021a96e2a797bcb2c412c62f645ff166af11700
Evan Prock <evan.prock@gmail.com>
2024-09-11 23:12:23 -0700

    shift click to flag, serious work on custom level done

--- main/leveldata.lua
+++ main/leveldata.lua
@@ -6,7 +6,7 @@ D.header = {"-TINY-", "-EASY-", "-NORMAL-", "-HARD-", "-INSANE-", "-CUSTOM-"}
 
 D.description = {"[fill this in]", "OG minefinder size", "[fill this in]", "grab the glasses", "patience is KEY", "~[max: 32x32]"}
 
-D.mine_nums = {10, 14, 40, 100, 150, 40}
+D.mine_nums = {10, 14, 40, 99, 149, 40}
 D.size_nums = {{8, 10, 16, 16, 24, 16}, {8, 10, 16, 30, 30, 16}}
 
 D.diff = 3
