16952042972f70925f5a04078f9ba821c02c2566
Evan Prock <evan.prock@gmail.com>
2024-09-18 10:23:46 -0700

    mac/pc sync

--- main/leveldata.lua
+++ main/leveldata.lua
@@ -5,7 +5,7 @@ D.custom_hs_list = {{{}}}
 
 D.header = {"-TINY-", "-EASY-", "-NORMAL-", "-HARD-", "-INSANE-", "-CUSTOM-"}
 
-D.description = {"[fill this in]", "OG minefinder size", "[fill this in]", "grab the glasses", "patience is ~~~~~~~~~~~~~~~", "~[max: 32x32]"}
+D.description = {"pipsqueak mode", "OG minefinder size", "[fill this in]", "~get hard stay hard", "patience is ~~~~~~~~~~~~~~~", "~[max: 32x32]"}
 
 D.mine_nums = {10, 14, 40, 99, 149, 40}
 D.size_nums = {{8, 10, 16, 16, 24, 16}, {8, 10, 16, 30, 30, 16}}
