01deb408ee1bf2e278bbc1d3136665f6907af15d
Evan Prock <evan.prock@gmail.com>
2025-02-04 20:33:35 -0800

    deep polishing of control scroll

--- main/leveldata.lua
+++ main/leveldata.lua
@@ -1,7 +1,6 @@
 local D = {}
 
 D.in_game = false -- tracks if the splashscreen is finished
-
 D.os = "mac"
 
 D.highscore = {}
@@ -16,6 +15,8 @@ D.size_nums = {{8, 10, 16, 16, 24, 16}, {8, 10, 16, 30, 30, 16}}
 
 D.diff = 3
 D.diff_temp = nil 
+
+D.gui_anims = true
 
 D.settings = {tutorial = true, profanity = false, autosave = true, dig_around = true, scroll = true, scroll_slider = 3, sfx_slider = 4, dig_key = true, graffiti = true, splashscreen = true, gamer_lights = false}
 D.more_settings = false
